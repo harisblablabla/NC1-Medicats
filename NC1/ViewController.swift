cfccf6ceea51d32c0c0ec8a21b34f8721ca62be2
haris <haris.fadhilah>
2022-04-28 16:04:40 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Haris Fadhilah on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
